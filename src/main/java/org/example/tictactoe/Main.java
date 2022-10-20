9751c4ae3c05c7ef35e87bbff417acb50805689a
Jayavardhan <jayavardhan.work@gmail.com>
2022-10-20 09:27:37 +0530

    basic setup

---
+++ src/main/java/org/example/tictactoe/Main.java
@@ -0,0 +1,7 @@
+package org.example.tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/tictactoe/Player.java
@@ -0,0 +1,9 @@
+package org.example.tictactoe;
+
+public class Player {
+    private String character;
+
+    public Player(String character) {
+        this.character = character;
+    }
+}
